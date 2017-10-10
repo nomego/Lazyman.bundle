accddb9d2955a1aad17025ff86593d0cb8590fa2
Jason O'Neal <jason@jasononeal.net>
2017-10-09 22:15:06 -0400

    Fix quality settings to apply to recaps

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -121,19 +121,20 @@ def getRecapVCO(date, type, recap, sport):
 		objects = []
 		for video in videos:
 			bitrate = int(video["name"].split("_")[1][0:-1])
-			height = int(video["height"])
-			objects.insert(0, MediaObject(
-				container = Container.MP4,
-				video_codec = VideoCodec.H264,
-				audio_codec = AudioCodec.AAC,
-				video_resolution = height,
-				audio_channels = 2,
-				height = height,
-				width = int(video["width"]),
-				parts = [
-					PartObject(key=Callback(PlayRecap, url=video["url"]))
-				]
-			))
+			if Prefs['quality'] != 'Auto' and Prefs['quality'] == video["height"]+'p':
+				height = int(video["height"])
+				objects.insert(0, MediaObject(
+					container = Container.MP4,
+					video_codec = VideoCodec.H264,
+					audio_codec = AudioCodec.AAC,
+					video_resolution = height,
+					audio_channels = 2,
+					height = height,
+					width = int(video["width"]),
+					parts = [
+						PartObject(key=Callback(PlayRecap, url=video["url"]))
+					]
+				))
 		return objects
 
 	return VideoClipObject(
